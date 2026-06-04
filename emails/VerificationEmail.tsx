cc1a9802df8b1b7436481b0800f40c9c55982ba5
Satyam Dixit <satyamdixit2003@gmail.com>
2026-06-05 00:21:23 +0530

    setup models

---
+++ emails/VerificationEmail.tsx
@@ -0,0 +1,56 @@
+import { Body, Button, Container, Head, Heading, Html, Img, Preview, Section, Tailwind, Text } from 'react-email';
+
+interface WelcomeEmailProps {
+  username?: string;
+  otp?: string;
+}
+
+const WelcomeEmail = ({
+  username = 'Nicole',
+  otp = '5663636',
+}: WelcomeEmailProps) => {
+  const previewText = `Welcome  ${username}!`;
+
+  return (
+    <Html>
+      <Head />
+      <Preview>{previewText}</Preview>
+      <Tailwind>
+        <Body className="bg-black m-auto font-sans">
+          <Container className="mb-10 mx-auto p-5 max-w-[465px]">
+            <Section className="mt-10">
+              <Img
+                src={`https://example.com/brand/example-logo.png`}
+                width="60"
+                height="60"
+                alt="Logo Example"
+                className="my-0 mx-auto"
+              />
+            </Section>
+            <Heading className="text-2xl text-white font-normal text-center p-0 my-8 mx-0">
+               {username}!
+            </Heading>
+            <Text className="text-start text-sm text-white">
+              Hello {username},
+            </Text>
+            <Text className="text-start text-sm text-white leading-relaxed">
+            Thankyou {username} fpr registration on our application . Please use the following verification code to complete your
+            registration : {otp}
+            </Text>
+            <Section className="text-center mt-[32px] mb-[32px]">
+              <Button
+                className="py-2.5 px-5 bg-white rounded-md text-black text-sm font-semibold no-underline text-center"
+                href={`https://example.com/get-started`}
+              >
+                Get Started
+              </Button>
+            </Section>
+          
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+export default WelcomeEmail;
---
+++ src/helper/sendEmailVerification.ts
@@ -0,0 +1,12 @@
+import { resend } from "@/lib/resend";
+
+ import VerificationEmail from '../../emails/VerificationEmail'
+
+ import { ApiResponse } from "@/types/ApiResponses";
+
+ export async function sendVerificationEmail(
+     email :string,
+    username:string,
+    verifyCode: string
+
+ ): Promise <ApiResponse>{}
