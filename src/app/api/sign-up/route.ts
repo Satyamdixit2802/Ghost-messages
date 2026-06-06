e4972b2a0d7dbe598936b1a19a0bdb6e05962ce3
Satyam Dixit <satyamdixit2003@gmail.com>
2026-06-06 23:22:29 +0530

    handle userverification

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,15 +1,92 @@
 import dbConnect from '@/lib/dbConnect'
-import UserMode from '@/models/User.model'
+import UserModel from '@/models/User.model'
 import bcrypt from 'bcryptjs'
 
-import {sendEmailVerification} from '@/helper/sendEmailVerification'
+import {sendVerificationEmail} from '@/helper/sendEmailVerification'
 import { success } from 'zod';
+
 
 export async function POST (req: Request){
     await dbConnect()
 
     try {
         const {username, email, password} =   await req.json()
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified:true
+        })
+
+         if(existingUserVerifiedByUsername){
+            return Response.json({
+                success : false,
+                message : "Username is already taken"
+            },{
+                status : 400 
+            })
+         }
+
+         const existingUserVerifiedByEmail = await UserModel.findOne({email})
+
+         const verifyCodeOTP = Math.floor(100000 + Math.random() * 900000).toString()
+
+         if(existingUserVerifiedByEmail){
+            if(existingUserVerifiedByEmail.isVerified){
+                return Response.json({
+                    success : false,
+                    message : "User already exist with this email"
+                },{status : 400})
+            } else {
+                   const hashPassword = await bcrypt.hash(password,10)
+                   existingUserVerifiedByEmail.password =  hashPassword;
+                   existingUserVerifiedByEmail.verifyCode = verifyCodeOTP;
+                   existingUserVerifiedByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+
+                   await existingUserVerifiedByEmail.save()
+
+            }
+
+         }else{
+            const hashPassword = await bcrypt.hash(password,10)
+            const expiryDate = new Date()
+            expiryDate.setHours(expiryDate.getHours()+1)
+
+           const newUser =  new UserModel({
+                username,
+                email,
+                password : hashPassword,
+                verifyCode : verifyCodeOTP,
+                verifyCodeExpiry: expiryDate,
+                isVerified : false,
+                isAcceptingMessage : true,
+                messages : []
+            })
+            await newUser.save()
+         }
+
+         //send Verification email
+
+         const emailResponse  = await sendVerificationEmail(
+            email,
+            username,
+            verifyCodeOTP
+         )
+
+         if(!emailResponse.success){
+            return Response.json({
+                success : false,
+                message : emailResponse.message
+            }, {
+                status : 500
+            })
+         }
+
+         return Response.json({
+            success : true,
+            message : "User registered successfully. Please verify your email"
+         }, {
+            status : 201
+         })
+
     } catch (error) {
         console.error('Error registering user',error)
         return  Response.json({
