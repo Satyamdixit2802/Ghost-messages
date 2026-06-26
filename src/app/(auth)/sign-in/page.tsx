96be163f686691a4db1cacd966243eaf5e391584
Satyam Dixit <satyamdixit2003@gmail.com>
2026-06-26 19:47:16 +0530

    seeting up sign-in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,28 +1,57 @@
-'use client'
+"use client";
 
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
 
-export default function Component() {
-  const { data: session } = useSession()
+import * as z from 'zod'
+import Link from 'next/link'
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroupTextarea,
+} from "@/components/ui/input-group"
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import {useDebounceValue} from 'usehooks-ts'
+import { toast } from "sonner"
+import { useRouter } from 'next/router'
 
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user?.email}
-        <br />
-        <button onClick={() => signOut()}>
-          Sign out
-        </button>
-      </>
-    )
-  }
+const Page = () => {
+  
+   const router = useRouter()
+  const [username, setUsername] = useState('');
+  const [usernameMessage, setUsernameMessage] = useState('');
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting,setIsSubmitting] = useState(false);
+  const debouncedValue = useDebounceValue(username,300 )  
 
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>
-        Sign in
-      </button>
-    </>
-  )
-}
+    <div>
+       <Button
+        variant="outline"
+        onClick={() => toast.error("Event has not been created")}
+      >
+        Error
+      </Button>
+    </div>
+  );
+};
+
+export default Page;
